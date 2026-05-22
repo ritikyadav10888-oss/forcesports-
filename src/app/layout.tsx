aeedaf69c4435d1333dddfadab690adbc4baf5df
deathalone215 <deathalone215@gmail.com>
2026-05-22 19:13:48 +0530

    feat: add Google Analytics (G-QYY8VZXZS2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "../index.css";
 import PublicLayoutWrapper from "../components/layout/PublicLayoutWrapper";
 import { Analytics } from "@vercel/analytics/react";
 import Schema from "../components/seo/Schema";
+import Script from "next/script";
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700', '900'],
@@ -36,6 +37,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <PublicLayoutWrapper>
           {children}
         </PublicLayoutWrapper>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QYY8VZXZS2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QYY8VZXZS2');
+          `}
+        </Script>
         <Analytics />
         <Schema />
       </body>
